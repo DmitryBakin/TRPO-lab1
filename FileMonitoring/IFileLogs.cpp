a735cc8e80feb3059617b313bf3b20a589aaa7a9
Dmitry Bakin <dima.bakin2017@yandex.ru>
2026-03-18 12:17:54 +0700

    New classes added
    Added the PrintInfoToConsole and PrintInfoToOtherFile classes, which inherit from IFileLogs. For these classes, the print method was implemented.

--- FileMonitoring/IFileLogs.cpp
+++ FileMonitoring/IFileLogs.cpp
@@ -1,3 +1,26 @@
-#include "FileLogs.h"
+#include <iostream>
 
-FileLogs::FileLogs() {}
+#include "IFileLogs.h"
+
+void PrintInfoToConsole::print(QVector<File> files)
+{
+    for(int i = 0; i < files.size(); i++)
+    {
+        std::cout << QDateTime::currentDateTime().toString("dd.MM.yyyy hh:mm:ss").toStdString() << " Path: " << files[i].filePath().toStdString() << " exists: " << files[i].exists() << " size: " << files[i].size() << std::endl;
+    }
+}
+
+void PrintInfoToOtherFile::print(QVector<File> files)
+{
+
+    QFile file("C:/TRPO-lab1-Development/output.txt");
+    if (file.open(QFile::WriteOnly | QFile::Append | QFile::Text))
+    {
+        QTextStream out(&file);
+        for(int i = 0; i < files.size(); i++)
+        {
+            out << QDateTime::currentDateTime().toString("dd.MM.yyyy hh:mm:ss") << " Path: " << files[i].filePath() << " exists: " << files[i].exists() << " size: " << files[i].size() << "\n";
+        }
+    }
+    file.close();
+}
--- FileMonitoring/IFileLogs.h
+++ FileMonitoring/IFileLogs.h
@@ -1,6 +1,30 @@
-class FileLogs
+#include <QDateTime>
+#include <QFile>
+#include <QTextStream>
+
+
+#include "File.h"
+
+class IFileLogs
 {
 public:
-    FileLogs();
+    virtual ~IFileLogs() = default;
+
+    virtual void print(QVector<File> files) = 0;
+};
+
+class PrintInfoToConsole : public IFileLogs
+{
+    virtual ~PrintInfoToConsole() override = default;
+
+    void print(QVector<File> files) override;
+};
+
+class PrintInfoToOtherFile : public IFileLogs
+{
+    virtual ~PrintInfoToOtherFile() override = default;
+
+    void print(QVector<File> files) override;
+
 };
 
