4a1339f4923b921c2a851e756a3285a2a2984fdc
Dmitry Bakin <dima.bakin2017@yandex.ru>
2026-05-08 13:12:44 +0700

    Refactoring

--- FileMonitoring/main.cpp
+++ FileMonitoring/main.cpp
@@ -9,46 +9,47 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    bool initialStatePrinted = false;
-
     QTimer *timer = new QTimer(&a);
     timer->setInterval(100);
 
-    QObject::connect(timer, &QTimer::timeout, &a, [&](){
-         QString appDir = QCoreApplication::applicationDirPath();
+    QObject::connect(timer, &QTimer::timeout, &a, []()
+     {
+        static bool initialStatePrinted = false;
 
-         QDir projectDir(appDir);
-         projectDir.cdUp();
-         projectDir.cdUp();
-         projectDir.cdUp();
+        QString appDir = QCoreApplication::applicationDirPath();
 
-         QString filePath = projectDir.absolutePath() + "/input.txt";
-         QFile inputFile(filePath);
+        QDir projectDir(appDir);
+        projectDir.cdUp();
+        projectDir.cdUp();
 
-         if (inputFile.open(QFile::ReadOnly | QFile::Text))
-         {
-             QTextStream stream(&inputFile);
-             QString fileNames = stream.readAll();
-             inputFile.close();
 
-             QStringList list = fileNames.split(",");
-             FileManager& instance = FileManager::Instance();
-             instance.clear();
-             instance.addFiles(list);
-         }
-         FileManager& instance = FileManager::Instance();
-         QVector<QFileInfo> currStates = instance.getFileVector();
+        QString filePath = projectDir.absolutePath() + "/input.txt";
+        QFile inputFile(filePath);
 
-         if(!initialStatePrinted)
-         {
-             for(int i = 0; i < currStates.size(); i++)
-                 qDebug() << QDateTime::currentDateTime().toString("dd.MM.yyyy hh:mm:ss")
-                          << "Path:" << currStates[i].filePath()
-                          << "Exists:" << currStates[i].exists()
-                          << "Size:" << currStates[i].size();
-             initialStatePrinted = true;
-         }
-     });
+        if (inputFile.open(QFile::ReadOnly | QFile::Text))
+        {
+            QTextStream stream(&inputFile);
+            QString fileNames = stream.readAll();
+            inputFile.close();
+
+            QStringList list = fileNames.split(",");
+            FileManager& instance = FileManager::Instance();
+            instance.clear();
+            instance.addFiles(list);
+        }
+        FileManager& instance = FileManager::Instance();
+        QVector<QFileInfo> currStates = instance.getFileVector();
+
+        if(!initialStatePrinted)
+        {
+            for(int i = 0; i < currStates.size(); i++)
+                qDebug() << QDateTime::currentDateTime().toString("dd.MM.yyyy hh:mm:ss")
+                        << "Path:" << currStates[i].filePath()
+                        << "Exists:" << currStates[i].exists()
+                        << "Size:" << currStates[i].size();
+            initialStatePrinted = true;
+        }
+    });
 
     timer->start();
 
